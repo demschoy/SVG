53a2d81ec02934b43d7b3ef819dd7cdd7b864d8b
Demschoy <demschoy@gmail.com>
2019-06-27 20:36:59 +0300

    Add default Point constructor

--- src/figures/Point.hpp
+++ src/figures/Point.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
+#define DEFAULT_X 0.0
+#define DEFAULT_Y 0.0
+
 struct Point
 {
 	double x;
 	double y;
 
-	Point(double _x, double _y);
+	Point(double _x = DEFAULT_X, double _y = DEFAULT_Y);
 	Point& operator=(const Point &other);
 
 	void print();
