58c01472a76833ee56e1064b26e0a15f041f3055
Demschoy <demschoy@gmail.com>
2019-06-28 23:08:55 +0300

    Add Menu class

---
+++ src/commandLines/Menu.cpp
@@ -0,0 +1,207 @@
+#include "Menu.hpp"
+
+#include <iostream>
+#include <fstream>
+
+void Menu::program()
+{
+	std::cout << welcomeMessage << operationList;
+	std::cout << operationsMessage;
+
+	std::cin >> commandCode;
+	while (commandCode != 8)
+	{
+		switch (commandCode)
+		{
+		case 0: operationOpen(); break;
+		case 1: operationLoad(); break;
+		case 2: operationCreate();  break;
+		case 3: operationErase(); break;
+		case 4: operationWithin(); break;
+		case 5: operationTranslate(); break;
+		case 6: operationPrint(); break;
+		case 7: operationClose(); break;
+		default:
+			invalidOperation();
+		}
+		std::cin >> commandCode;
+	}
+
+}
+
+void Menu::operationOpen()
+{
+	if (isOpened)
+	{
+		std::cout << alreadyOpened;
+		return;
+	}
+	
+	std::cout << enterFileName;
+	std::cin >> fileName;
+	command.openFile(fileName, file);
+	isOpened = true;
+	std::cout << successfullyOpened << fileName << std::endl;
+	
+	std::cout << suggestedLoading;
+}
+
+void Menu::operationLoad()
+{
+	if (isLoaded)
+	{
+		std::cout << alreadyOpened;
+		return;
+	}
+	
+	command.loadFile(file);
+	isLoaded = true;
+	std::cout << successfullyLoaded << fileName << std::endl;
+	std::cout << continueMessage;
+}
+
+void Menu::operationCreate()
+{
+	std::cout << enterCreatedFigureType;
+	std::string type;
+	std::cin >> type;
+	
+	std::cout << enterColor;
+	std::string color;
+	std::cin >> color;
+
+	if (type == CIRCLE)
+	{
+		Point coordinates;
+		double radius;
+		enterCircle(coordinates, radius);
+		command.createCircle(coordinates, radius, color);
+	}
+	if (type == RECTANGLE)
+	{
+		Point coordinates;
+		double width, height;
+		enterRectangle(coordinates, width, height);
+	}
+	if (type == ELLIPSE)
+	{
+		Point coordinatesCenter, radius;
+		enterEllipse(coordinatesCenter, radius);
+		command.createEllipse(coordinatesCenter, radius, color);
+	}
+}
+
+void Menu::operationErase()
+{
+	std::cout << enterErasePosition;
+	int position;
+	std::cin >> position;
+	
+	command.erase(position);
+}
+
+void Menu::operationWithin()
+{
+	std::cout << enterWithinFigure;
+	std::string figureType;
+	std::cin >> figureType;
+
+	if (figureType == RECTANGLE)
+	{
+		Point coordinates;
+		double width, height;
+		enterRectangle(coordinates, width, height);
+
+		std::vector<Figure*> withinFigures = command.withinRectangle(coordinates, width, height);
+		printWithinFigures(withinFigures);
+	}
+	if (figureType == CIRCLE)
+	{
+		Point coordinates;
+		double radius;
+		enterCircle(coordinates, radius);
+
+		std::vector<Figure*> withinFigures = command.withinCircle(coordinates, radius);
+		printWithinFigures(withinFigures);
+	}
+}
+
+void Menu::printWithinFigures(std::vector<Figure*> withinFigures)
+{
+	int size = withinFigures.size();
+	if (size == 0)
+	{
+		std::cout << noFiguresWithin;
+		return;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		withinFigures[i]->print();
+	}
+}
+
+void Menu::operationTranslate()
+{
+	std::cout << enterTranslatedFigureType;
+	std::string type;
+	std::cin >> type;
+
+	std::cout << std::endl << enterTranslatedCoordinates;
+	Point translated;
+	std::cin >> translated.x >> translated.y;
+	
+	command.translate(translated, type);
+}
+
+void Menu::operationPrint()
+{
+	command.print();
+}
+
+void Menu::operationClose()
+{
+	command.closeFile(file);
+	command.deleteFigures();
+	isOpened = false;
+	std::cout << successfullyClosed << fileName << std::endl;
+}
+
+void Menu::invalidOperation()
+{
+	std::cout << invalidOperationSelected;
+}
+
+void Menu::enterRectangle(Point &coordinates, double &width, double &height)
+{
+	std::cout << enterRectangleCoordinates;
+	std::cin >> coordinates.x >> coordinates.y;
+
+	std::cout << std::endl << enterWidth;
+	std::cin >> width;
+
+	std::cout << std::endl << enterHeight;
+	std::cin >> height;
+}
+
+void Menu::enterCircle(Point & centerCoordinates, double &radius)
+{
+	std::cout << enterCircleCoordinates;
+	std::cin >> centerCoordinates.x >> centerCoordinates.y;
+
+	std::cout << std::endl << enterRadius;
+	std::cin >> radius;
+}
+
+void Menu::enterEllipse(Point &centerCoordinates, Point &radius)
+{
+	std::cout << enterEllipseCoordinates;
+	Point coordinates;
+	std::cin >> centerCoordinates.x >> centerCoordinates.y;
+
+	std::cout << std::endl << enterEllipseRadius;
+	std::cin >> radius.x >> radius.y;
+
+}
+
+
---
+++ src/commandLines/Menu.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+#include "Command.hpp"
+
+#define RECTANGLE "rectangle"
+
+class Menu
+{
+	void program();
+
+private:
+	Command command;
+	int commandCode;
+	std::string fileName;
+	std::fstream file;
+
+	bool isOpened = false;
+	bool isLoaded = false;
+	std::string welcomeMessage = "Welcome to SVG. \n";
+	std::string operationList = "Please choose operation: \n";
+	std::string openMessage = "For OPEN enter: 0\n";
+	std::string loadMessage = "For LOAD enter: 1\n";
+	std::string createMessage = "For CREATE enter 2\n";
+	std::string eraseMessage = "For ERASE enter 3\n";
+	std::string withinMessage = "For WITHIN enter 4\n";
+	std::string translateMessage = "For TRANSLATE enter 5\n";
+	std::string printMessage = "For PRINT enter 6\n";
+	std::string closeMessage = "For CLOSE enter 7\n";
+	std::string exitMessage = "For EXIT enter 8\n";
+	
+	std::string invalidOperationSelected = "The selected operation is invalid. Please try again.\n";
+	std::string alreadyLoaded = "File is already loaded.\n";
+	std::string alreadyOpened = "File is already opened.\n";
+
+	std::string continueMessage = "Do you want to continue? Enter from 0 from 8.\n";
+	
+	std::string enterFileName = "Please enter file name: ";
+	std::string enterErasePosition = "Please enter the position of the figure to be erased: ";
+	std::string enterWithinFigure = "Please enter within figure - rectangle or circle: ";
+	std::string enterRectangleCoordinates = "Please enter rectangle coordinates: ";
+	std::string enterWidth = "Please enter rectangle width: ";
+	std::string enterHeight = "Please enter rectangle height: ";
+	std::string enterCircleCoordinates = "Please enter circle center coordinates: ";
+	std::string enterRadius = "Please enter circle radius: ";
+	std::string enterEllipseCoordinates = "Please enter ellipse center: ";
+	std::string enterEllipseRadius = "Please enter ellipse radiuses: ";
+	std::string enterTranslatedFigureType = "Please enter figure type to be translated - circle, rect, ellipse or all figures: ";
+	std::string enterTranslatedCoordinates = "Please enter coordinates of the translating point: ";
+	std::string enterCreatedFigureType = "Please enter a figure you want to create - circle, rectangle, ellipse: ";
+	std::string enterColor = "Please enter color: ";
+
+	std::string successfullyOpened = "Successfully opened file ";
+	std::string successfullyLoaded = "Successfully loaded file ";
+	std::string successfullyClosed = "Successfully closed file ";
+	
+	std::string suggestedLoading = "If you want to load the file, please enter 1.";
+	
+	std::string noFiguresWithin = "No figures are located within the figure\n";
+
+	std::string operationsMessage = openMessage + loadMessage + createMessage + eraseMessage + withinMessage +
+		translateMessage + printMessage + closeMessage + exitMessage; 
+
+	void operationOpen();
+	void operationLoad();
+	void operationCreate();
+	void operationErase();
+	void operationWithin();
+	void printWithinFigures(std::vector<Figure*> withinFigures);
+	void operationTranslate();
+	void operationPrint();
+	void operationClose();
+	void invalidOperation();
+
+	void enterRectangle(Point &coordinates, double &width, double &height);
+	void enterCircle(Point &centerCoordinates, double &radius);
+	void enterEllipse(Point &centerCoordinates, Point &radius);
+};
